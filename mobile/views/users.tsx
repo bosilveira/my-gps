d94e45b4d96eb71cc11db504c85ff28e5aea2017
bosilveira <bosilveira@gmail.com>
2023-02-17 16:41:41 -0300

    feat: api and redux

--- mobile/views/users.tsx
+++ mobile/views/users.tsx
@@ -4,12 +4,50 @@ import { StatusBar } from 'expo-status-bar';
 import { Text } from 'react-native-paper';
 import { SegmentedButtons } from 'react-native-paper';
 import { Appbar, Button } from 'react-native-paper';
-import UserList from '../components/userList';
+
+import { Card, Avatar, Divider, ActivityIndicator } from 'react-native-paper';
+import { Switch, IconButton, List, Chip } from 'react-native-paper';
+
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import type { RootStackParamList } from '../App';
 type Props = NativeStackScreenProps<RootStackParamList, 'Users'>;
 
+import { AppDispatch, RootState } from '../redux/store.redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { getUsers } from '../redux/userSlices.redux';
+
 export default function UsersPage({ navigation }: Props) {
+  
+  const dispatch = useDispatch<AppDispatch>();
+  const login = useSelector((state: RootState) => state.login);
+  const userData = useSelector((state: RootState) => state.user);
+
+  const createCards = (data: any[]) => {
+    const list = []
+    for (let i = 0; i < data?.length; i++) {
+    let jsx = <>
+        <List.Item 
+        onPress={() => navigation.push('User')} 
+        key={'card'+i}
+        title={() => <Text variant="labelLarge">{data[i].name}</Text>}
+        description={() =><>
+        <Text variant="labelMedium">{["Administrator", "Manager", "Courier", "Driver", "CSR"][data[i].role]}</Text>
+        <Text variant="labelMedium">{data[i].available ? "Available" : "Unavailable"}</Text>
+        <Text variant="labelMedium">{data[i].status}</Text>
+        </>}
+        left={() => <List.Icon icon="car" />}
+        right={() => <List.Icon icon="arrow-right-drop-circle-outline"/>}
+        style={{marginLeft: 16, marginVertical: 8}}
+        />
+        <Divider style={{marginVertical: 8}} />
+        </>;
+
+    list.push(jsx);
+    }
+    return list;
+}
+
+
   return (
     <>
     <StatusBar 
@@ -19,10 +57,18 @@ export default function UsersPage({ navigation }: Props) {
     <Appbar.Header>
         <Appbar.BackAction onPress={() => navigation.navigate('Home')} />
         <Appbar.Content title="Users" />
-        <Appbar.Action icon="account-multiple" onPress={() => {}} />
+        {userData.loading ? 
+        <Appbar.Action icon={()=><ActivityIndicator animating={true} />} /> :
+        <Appbar.Action icon="reload" onPress={()=>dispatch(getUsers())} /> }
+        <Appbar.Action icon="account-multiple" />
     </Appbar.Header>
     <Button icon="plus" mode="contained" onPress={() => navigation.push('AddUser')} style={{marginVertical: 16, marginHorizontal: 32}}>Add User</Button>
-    <UserList/>
+    <ScrollView>
+      <List.Section>
+          <Divider style={{marginTop: 0, marginBottom: 8}} />
+          {createCards(userData.list)}
+      </List.Section>
+    </ScrollView>
     </>
   );
 }
